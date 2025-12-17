c865bb1003f31dc51faa68a08206e0b5d5654029
Proudroombafather <enchantermcwarlock@gmail.com>
2025-12-17 16:55:30 -0600

    fix the problem after learning the solution

--- stats.py
+++ stats.py
@@ -1,23 +1,15 @@
-def get_book_text():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    word_count = len(file_contents.split())
-    print(f"Found {word_count} total words")
-
-def character_count():
-     with open("books/frankenstein.txt") as text:
-        # Gets the text of the book counts them and ties them to dictonary pairs, while also lowercasing them all.
-        character_count_dict = {}
-        for character in text:
-            if character.lower() not in character_count_dict:
-                character_count_dict[character.lower()] = 1
-            else:
-                character_count_dict[character.lower()] += 1
-        print(character_count_dict)
-
-def main():
-    get_book_text()
-    character_count()
+def get_num_words(text):
+    words = text.split()
+    return len(words)
 
 
-main()
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
